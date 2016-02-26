2a16d12063fb78f72a520128272575eddeb7c388
root <root@es3.es.com>
2016-02-26 04:03:01 -0600

    added code for event upload, modified collision upload

--- dataAcquisition/acquire_NYC_Collisions.py
+++ dataAcquisition/acquire_NYC_Collisions.py
@@ -21,11 +21,21 @@ def upload_collision_data(flatfile,index,doc_type):
         for row in csv_reader:
                 #add GEOJSON if lat and long exists
                 if row['LATITUDE'] != "" and row['LONGITUDE'] != "":
-                        row["GEOJSON_C"] = { "lat" : float(row['LATITUDE']), "lon": float(row["LONGITUDE"]) }
+                        row["GEOJSON_C"] = (float(row["LONGITUDE"]),float(row['LATITUDE']))
+			#row["GEOJSON_C"] = { "lat" : float(row['LATITUDE']), "lon": float(row["LONGITUDE"]) }
                 #convert date and time into one variable
                 row["DATETIME_C"] = datetime.strptime(row["DATE"] + " " + row["TIME"]+ ':00', "%m/%d/%Y %H:%M:%S")
                 #assign a unique id based on date, time, and location
                 row["ID"] = hashlib.sha224(row["DATE"] + row["TIME"] + row["LATITUDE"] + row["LONGITUDE"]).hexdigest()
-                records.append(row)
 
-        upload_to_Elasticsearch.upload_docs_to_ES(records,index,doc_type,id_field="ID",geopoint="GEOJSON_C") 
+		# append "collision_" in front of each column
+		#print row
+		newrow = dict()
+		for key, value in row.iteritems():
+			newrow["collision_" + key] = value
+                #print newrow
+		
+			
+	  	records.append(newrow)	
+
+        upload_to_Elasticsearch.upload_docs_to_ES(records,index,doc_type,id_field="collision_ID",geopoint="collision_GEOJSON_C") 
